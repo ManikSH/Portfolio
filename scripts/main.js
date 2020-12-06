7315f969e3022755b707f4be589ecc53e4716af3
Manik <maniksh1981@gmail.com>
2020-12-06 16:40:41 +0530

    commit1

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,29 @@
-let hamburger = document.querySelector('.hamburger');
-let navLinks = document.querySelector('.navLinks');
+let hamburger = document.querySelector(".hamburger");
+let navLinks = document.querySelector(".navLinks");
+let links = document.querySelectorAll(".navLinks a");
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('removeNavLinks');
-    hamburger.classList.toggle('changeColor');
-    hamburger.classList.toggle("animateSticks");
+let animateSticks = () => {
+  hamburger.classList.toggle("animateSticks");
+};
+
+let openCloseNav = () => {
+  navLinks.classList.toggle("removeNavLinks");
+};
+
+let changeColor = () => {
+  hamburger.classList.toggle("changeColor");
+};
+
+hamburger.addEventListener("click", () => {
+  openCloseNav();
+  changeColor();
+  animateSticks();
 });
+
+for (link of links) {
+  link.addEventListener("click", () => {
+    changeColor();
+    animateSticks();
+    openCloseNav();
+  });
+}
